e6daff56ae461c9114227c08e7377adc37d88c6d
Hasarangakgnp <nimna.21@cse.mrt.ac.lk>
2024-09-04 23:27:34 +0530

    create user login

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -8,14 +8,28 @@ export const getUser = (req, res) => {
 }
 export const createUser = async (req, res) => {
     try{
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        console.log(salt);
-        console.log(hashedPassword);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const user = {name: req.body.name, password:hashedPassword}
         users.push(user);
         res.status(201).send();
     }catch{
+        res.status(500).send();
+    }
+}
+export const login = async(req, res) => {
+    
+    const user = users.find(user => user.name = req.body.name)
+    if (user==null){
+        return res.status(400).send('Cannot find user');
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+            res.send('Success'); 
+        } else {
+            res.send('Not allowed')
+        }
+
+    } catch {
         res.status(500).send();
     }
 }
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {getUser, createUser} from '../controllers/usersControllers.js'
+import {getUser, createUser, login} from '../controllers/usersControllers.js'
 
 const router = express.Router();
 
 router.get('/', getUser);
 router.post('/', createUser);
+router.post('/login', login);
 
 
 export default router;
